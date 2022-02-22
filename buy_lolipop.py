54f6a44c199135872564ac1b17cee74a7be9e08e
xiaolu <2487302491@qq.com>
2022-02-22 09:55:23 +0800

    第一次作业

---
+++ buy_lolipop.py
@@ -0,0 +1,9 @@
+def cal_amount_of_lolipop():
+    # your code here ^_^
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    c = a + b / 10
+    answer = int(c // 6.7)
+    print(answer)
+    return
